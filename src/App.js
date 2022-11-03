d8ad25e45001f0bfb4608e48e3089dd974a60e80
frederic-neo <frederic@neoito.com>
2022-11-03 16:11:57 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import User_list from './user_list'
+export default function App() {
+  return (
+    <div>
+      <User_list/>
+    </div>
+  )
+}
---
+++ src/user_list.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import env from 'env'
+export const User_list = () => {
+  return (
+   <p>Hello from user_list </p>
+  )
+}
+export default User_list
